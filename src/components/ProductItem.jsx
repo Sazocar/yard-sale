b29055eade4cfbb19cf339872817324012ccd2c8
Simón A Azócar <saimn.azocar@gmail.com>
2022-04-22 21:04:59 -0400

    added useGetProducts custom hook

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,7 +3,7 @@ import "@styles/ProductItem.scss";
 
 import addToCart from "@icons/bt_add_to_cart.svg";
 
-const ProductItem = () => {
+const ProductItem = ({product}) => {
   const [cart, setCart] = useState([]);
 
 	const handleClick = () => {
@@ -13,14 +13,14 @@ const ProductItem = () => {
   return (
     <div className="ProductItem">
       <img
-        src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        src={product.images[0]}
         className="Item-image"
         alt=""
       />
       <div className="ProductItem-info">
         <div>
-          <p>$ 120,00</p>
-          <p>Bike</p>
+          <p>$ {product.price}</p>
+          <p>{product.title}</p>
         </div>
         <figure onClick={handleClick}>
           <img src={addToCart} alt="" className="addToCart-image" />
