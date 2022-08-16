22bb32c8c1b8c7a6526e3c68c58de6512be10224
Simón A Azócar <saimn.azocar@gmail.com>
2022-08-16 18:52:14 -0400

    [FIX] webpack configs

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
 
   return (
     <AppContext.Provider value={initialState}>
-      <BrowserRouter>
+      <BrowserRouter basename="/yard-sale">
       <Layout>
         <Routes>
           <Route exact path="/" element={<Home />} />
